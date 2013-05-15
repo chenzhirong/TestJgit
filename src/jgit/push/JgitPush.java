4fc39a4dda4b22d48b08c8494b93a3c12cff6b03
chenzhirong <253494709@qq.com>
2013-05-15 16:44:31 +0800

    test tag

--- src/jgit/push/JgitPush.java
+++ src/jgit/push/JgitPush.java
@@ -2,6 +2,8 @@ package jgit.push;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PullCommand;
@@ -14,8 +16,12 @@ import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevObject;
+import org.eclipse.jgit.revwalk.RevTag;
+import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
@@ -52,19 +58,41 @@ public class JgitPush {
 		JgitPush.getGit();// 初始化GIT
 		try {
 			// 提交文件到本地
-			// commitConifgFile(new
-			// File("D:/cfrManage/ConfigFile/设备配置文件管理/思科/192.168.0.248/192.168.0.248confit.txt"));
+			// commitConifgFile(new File("D:/cfrManage/ConfigFile/设备配置文件管理/思科/192.168.0.248/192.168.0.248confit.txt"));
 			// testpush();//提交到远程
-			// testClone();//从远程克隆下来
-			boolean flg = createTag("ce2110fa2cf47c60868e1b0284c62968892e6d20",
-					"rongzai1", "rongzai521");
-			System.out.println(flg);
-			// testpull();
+			//testClone();//从远程克隆下来
+			//boolean flg = createTag("ce2110fa2cf47c60868e1b0284c62968892e6d20",
+					//"rongzai1", "rongzai521");
+			//System.out.println(flg);
+			//获取指定版本标签
+			System.out.println(getTag("ce2110fa2cf47c60868e1b0284c62968892e6d20"));
+			//删除标签
+			//deleteTag("rongzai");
+			 testpull();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		// createTag("ce2110fa2cf47c60868e1b0284c62968892e6d20","v1.6","version 1.6");
+	}
+	/**
+	 * 删除标签
+	 */
+	public static boolean deleteTag(String tagName) {
+		//Git git = Git.open(new File(gitPath));
+		try {
+			git.tagDelete().setTags(tagName).call();
+		} catch (ConcurrentRefUpdateException e) {
+		} catch (InvalidTagNameException e) {
+			e.printStackTrace();
+		} catch (NoHeadException e) {
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 	public static void init() {
@@ -150,7 +178,8 @@ public class JgitPush {
 	public static void testpush() throws InvalidRemoteException,
 			TransportException, GitAPIException, Exception {
 		PushCommand push = git.push();
-		push.setPushTags();
+		//push.setPushTags();
+		push.setPushAll();
 		push.setRemote(remotePath);
 		UsernamePasswordCredentialsProvider user = new UsernamePasswordCredentialsProvider(
 				UserName, UserPassword);
@@ -208,5 +237,37 @@ public class JgitPush {
 		}
 		return true;
 	}
+	/**
+	 * 得到标签名 通过Tag objectID 得到revCommit ObjectID
+	 */
+	public static String getTag(String version) {
+		String tagNameList = "";
+		RevWalk walk = null;
+		try {
+			Git git = Git.open(new File("D:/cfrManage/ConfigFile/.git"));
+			List<Ref> ref = git.tagList().call();
+			for (int i = 0; i < ref.size(); i++) {
+				Ref gitRef = ref.get(i);
+				ObjectId refId = gitRef.getObjectId();
+				walk = new RevWalk(git.getRepository());
+				RevObject revObject = walk.parseAny(refId);
+				if (revObject instanceof RevCommit) {
+
+				} else if (revObject instanceof RevTag) {
+					RevCommit revCommit = walk.parseCommit(revObject);
+					if (revCommit.getName().equals(version)) {
+						tagNameList = ((RevTag) revObject).getTagName() + "/"
+								+ tagNameList;
+					}
+
+				} else if (revObject instanceof RevTree) {
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tagNameList;
+	}
+
 
 }
